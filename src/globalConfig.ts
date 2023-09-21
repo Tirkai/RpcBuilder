92c14a1b4084d10bac3c6f2d3103f611b905d459
Дмитрий Буренков <dmitrii.burenkov@fortis.online>
2023-09-22 01:29:27 +0300

    Added Env Error handling

--- src/globalConfig.ts
+++ src/globalConfig.ts
@@ -6,17 +6,21 @@ class GlobalConfig {
   public serverPort = 8080;
 
   public load = async () => {
-    const { RPC_API_ENDPOINT_PATH, SERVER_PORT } = await loadEnv();
+    try {
+      const { RPC_API_ENDPOINT_PATH, SERVER_PORT } = await loadEnv();
 
-    this.rpcApiEndpointPath = EnvParser.getString({
-      value: RPC_API_ENDPOINT_PATH,
-      default: this.rpcApiEndpointPath,
-    });
+      this.rpcApiEndpointPath = EnvParser.getString({
+        value: RPC_API_ENDPOINT_PATH,
+        default: this.rpcApiEndpointPath,
+      });
 
-    this.serverPort = EnvParser.getInteger({
-      value: SERVER_PORT,
-      default: this.serverPort,
-    });
+      this.serverPort = EnvParser.getInteger({
+        value: SERVER_PORT,
+        default: this.serverPort,
+      });
+    } catch (e) {
+      console.error(e);
+    }
   };
 }
 
